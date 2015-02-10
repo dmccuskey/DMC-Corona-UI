c292bc029bc33596a6dd92e19c48617837a89849
dmc <my-github@davidmccuskey.com>
2015-02-10 00:00:55 -0700

    lots of updates to popover

--- lib/dmc_widgets/widget_popover.lua
+++ lib/dmc_widgets/widget_popover.lua
@@ -74,7 +74,7 @@ local Utils = require 'dmc_utils'
 local newClass = Objects.newClass
 local ComponentBase = Objects.ComponentBase
 
-local LOCAL_DEBUG = false
+local LOCAL_DEBUG = true
 
 
 
@@ -91,13 +91,16 @@ Popover.TOUCH_NONE = 'none'
 Popover.TOUCH_DONE = 'done'
 Popover.TOUCH_CANCEL = 'cancel'
 
+Popover.__WIDGET = nil -- widget manager
+
 
 --======================================================--
 -- Start: Setup DMC Objects
 
 function Popover:__init__( params )
 	-- print( "Popover:__init__" )
-	params = params or { }
+	params = params or {}
+	if params.outside_touch_action==nil then params.outside_touch_action=Popover.TOUCH_CANCEL end
 	self:superCall( '__init__', params )
 	--==--
 
@@ -105,9 +108,26 @@ function Popover:__init__( params )
 
 	if self.is_class then return end
 
+	assert( params.x_pos, "Popover: requires param 'x_pos'" )
+	assert( params.y_pos, "Popover: requires param 'y_pos'" )
+	assert( params.width, "Popover: requires param 'width'" )
+	assert( params.height, "Popover: requires param 'height'" )
+
 	--== Create Properties ==--
 
-	self._outsideTouchAction = params.outsideTouchAction or Popover.TOUCH_CANCEL
+	self._outside_touch_action = params.outside_touch_action
+
+	self._width = params.width
+	self._height = params.height
+
+	-- we don't move our core group
+	-- so we save x/y separately
+	self._x = 0
+	self._y = 0
+
+	-- this is the position of the activating button
+	self._x_pos = params.x_pos
+	self._y_pos = params.y_pos
 
 	self._onDone = params.onDone
 	self._onCancel = params.onCancel
@@ -123,8 +143,8 @@ function Popover:__init__( params )
 	--== Object References ==--
 
 	-- visual
-	self._bg_touch = nil  -- main background
-	self._bg_main = nil  -- main background
+	self._bg_touch = nil  -- main touch background
+	self._bg_main = nil  -- main visual background
 	self._pointer = nil -- pointer element
 
 end
@@ -144,9 +164,7 @@ function Popover:__createView__()
 	-- print( "Popover:__createView__" )
 	self:superCall( '__createView__' )
 	--==--
-
-	local WIDTH, HEIGHT = display.contentWidth, display.contentHeight
-
+	local WIDTH, HEIGHT = Popover.__WIDGET.WIDTH, Popover.__WIDGET.HEIGHT
 	local W,H = self._width, self._height
 	local H_CENTER, V_CENTER = W*0.5, H*0.5
 
@@ -157,38 +175,41 @@ function Popover:__createView__()
 	o = display.newRect(0, 0, WIDTH, HEIGHT)
 	o:setFillColor(0,0,0,0)
 	if LOCAL_DEBUG then
-		o:setFillColor(0,255,0,255)
+		o:setFillColor(1,0,0,0.5)
 	end
 	o.isHitTestable = true
 	o.anchorX, o.anchorY = 0,0
 	o.x, o.y = 0,0
 
-	self.view:insert( o )
+	self.view:insert( o ) -- using view because of override
 	self._bg_touch = o
 
+	-- background group
 
 	dg = display.newGroup()
-	self.view:insert( dg )
+	self.view:insert( dg ) -- using view because of override
 	self._dg_bg = dg
 
-	-- viewable background
-
-	o = display.newRect(0, 0, W, H)
-	o:setFillColor(1,1,1,0.8)
-	if LOCAL_DEBUG then
-		o:setFillColor(0,255,0,255)
-	end
-	o.anchorX, o.anchorY = 0,0
-	o.x, o.y = 0,0
-
-	dg:insert( o )
-	self._bg_main = o
-
-	--== setup main group
+	-- main group
 
 	dg = display.newGroup()
 	self.view:insert( dg )
 	self._dg_main = dg
+
+
+	-- viewable background
+
+	dg = self._dg_bg
+	o = display.newRect(0, 0, W, H)
+	o:setFillColor(1,1,1,0.8)
+	if LOCAL_DEBUG then
+		o:setFillColor(0,1,0,0.5)
+	end
+	o.anchorX, o.anchorY = 0.5,0
+	o.x, o.y = 0,0
+
+	dg:insert( o )
+	self._bg_main = o
 
 end
 
@@ -214,7 +235,11 @@ function Popover:__initComplete__()
 	o._f = self:createCallback( self._bgTouchEvent_handler )
 	o:addEventListener( 'touch', o._f )
 
-	self:_updateView()
+	-- use setters to test
+	self.onDone = self._onDone
+	self.onCancel = self._onCancel
+
+	self:_calculatePosition()
 end
 
 function Popover:__undoInitComplete__()
@@ -232,6 +257,15 @@ end
 --======================================================--
 
 
+--====================================================================--
+--== Static Methods
+
+function Popover.__setWidgetManager( manager )
+	-- print( "Popover.__setWidgetManager" )
+	Popover.__WIDGET = manager
+end
+
+
 
 --====================================================================--
 --== Public Methods
@@ -239,7 +273,7 @@ end
 
 -- we only want items inserted into proper layer
 function Popover:insert( ... )
-	print( "Popover:insert" )
+	-- print( "Popover:insert" )
 	self._dg_main:insert( ... )
 end
 
@@ -247,20 +281,51 @@ function Popover:show()
 	-- print( "Popover:show" )
 	self.view.isVisible = true
 	self._bg_touch.isHitTestable = true
+	self:_show()
 end
 function Popover:hide()
 	-- print( "Popover:hide" )
 	self.view.isVisible = false
 	self._bg_touch.isHitTestable = false
+	self:_hide()
 end
 
+function Popover.__getters:x( )
+	-- print( "Popover.__getters x'" )
+	return self._x
+end
 function Popover.__setters:x( value )
+	-- print( "Popover.__settersx x'", value )
+	assert( type(value)=='number' )
+	--==--
 	self._dg_bg.x = value
 	self._dg_main.x = value
+	self._x = value
+	self:_updateView()
+end
+function Popover.__getters:y( )
+	-- print( "Popover.__getters y '" )
+	return self._y
 end
 function Popover.__setters:y( value )
+	-- print( "Popover.__setters y'", value )
+	assert( type(value)=='number' )
+	--==--
 	self._dg_bg.y = value
 	self._dg_main.y = value
+	self._y = value
+	self:_updateView()
+end
+
+function Popover.__setters:onDone( func )
+	assert( func==nil or type(func)=='function' )
+	--==--
+	self._onDone = func
+end
+function Popover.__setters:onCancel( func )
+	assert( func==nil or type(func)=='function' )
+	--==--
+	self._onCancel = func
 end
 
 
@@ -269,22 +334,37 @@ end
 --== Private Methods
 
 
+function Popover:_show()
+	print( "OVERRIDE Popover:_show" )
+end
+
+function Popover:_hide()
+	print( "OVERRIDE Popover:_hide" )
+end
+
 function Popover:_updateView()
-	print( "Popover:_updateView" )
+	print( "OVERRIDE Popover:_updateView" )
 end
 
 function Popover:_doCancelCallback()
-	print( "Popover:_doCancelCallback" )
+	-- print( "Popover:_doCancelCallback" )
 	if type(self._onCancel)~='function' then return end
 	local event = {}
 	self._onCancel( event )
 end
 
 function Popover:_doDoneCallback()
-	print( "Popover:_doDoneCallback" )
+	-- print( "Popover:_doDoneCallback" )
 	if type(self._onDone)~='function' then return end
 	local event = {}
 	self._onDone( event )
+end
+
+-- TODO: make fancier
+function Popover:_calculatePosition()
+	local _x, _y = self._x_pos, self._y_pos
+
+	self.x, self.y = _x, _y
 end
 
 
@@ -294,7 +374,7 @@ end
 
 
 function Popover:_bgTouchEvent_handler( event )
-	print( "Popover:_bgTouchEvent_handler", event.phase )
+	-- print( "Popover:_bgTouchEvent_handler", event.phase )
 	local target = event.target
 
 	if event.phase == 'began' then
@@ -305,9 +385,9 @@ function Popover:_bgTouchEvent_handler( event )
 	if not self._has_focus then return end
 
 	if event.phase == 'ended' or event.phase == 'canceled' then
-		if self._outsideTouchAction == Popover.TOUCH_DONE then
+		if self._outside_touch_action == Popover.TOUCH_DONE then
 			self:_doDoneCallback()
-		elseif self._outsideTouchAction == Popover.TOUCH_CANCEL then
+		elseif self._outside_touch_action == Popover.TOUCH_CANCEL then
 			self:_doCancelCallback()
 		else
 			-- pass
