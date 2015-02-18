0cb3a083d8593f4b425a9ac8bf0a65217911e078
dmc <my-github@davidmccuskey.com>
2015-02-18 04:09:30 -0700

    updates to text theme example

--- examples/text-widget/text-themed/main.lua
+++ examples/text-widget/text-themed/main.lua
@@ -29,13 +29,31 @@ local Widgets = require 'lib.dmc_widgets'
 local W, H = display.contentWidth, display.contentHeight
 local H_CENTER, V_CENTER = W*0.5, H*0.5
 
-local text
-local theme, style
-
 
 
 --===================================================================--
 -- Support Functions
+
+
+-- Setup Visual Screen Items
+--
+local function setupBackground()
+	local width, height = 100, 50
+	local o
+
+	o = display.newRect(0,0,W,H)
+	o:setFillColor(0.5,0.5,0.5)
+	o.x, o.y = H_CENTER, V_CENTER
+
+	o = display.newRect(0,0,width+4,height+4)
+	o:setStrokeColor(0,0,0)
+	o.strokeWidth=2
+	o.x, o.y = H_CENTER, V_CENTER
+
+	o = display.newRect( 0,0,10,10)
+	o:setFillColor(1,0,0)
+	o.x, o.y = H_CENTER, V_CENTER
+end
 
 
 local function widgetOnPropertyEvent_handler( event )
@@ -63,150 +81,194 @@ end
 --===================================================================--
 
 
-local width, height = 100, 36
-local o
-
---== Setup Visual Screen Items
-
-o = display.newRect(0,0,W,H)
-o:setFillColor(0.5,0.5,0.5)
-o.x, o.y = H_CENTER, V_CENTER
-
-o = display.newRect(0,0,width+4,height+4)
--- o:setFillColor(1,0,0)
-o:setStrokeColor(0,0,0)
-o.strokeWidth=2
-o.x, o.y = H_CENTER, V_CENTER
-
-o = display.newRect( 0,0,10,10)
-o:setFillColor(1,0,0)
-o.x, o.y = H_CENTER, V_CENTER
+setupBackground()
 
 
---== Setup Widget
 
-
+--======================================================--
 --== create widget, default style
 
-text1 = Widgets.newText{}
-text1.text = "default style"
-text1.x, text1.y = H_CENTER, 25
+function run_example1()
+
+	local txt1
+
+	txt1 = Widgets.newText{}
+	txt1.text = "default style"
+	txt1.x, txt1.y = H_CENTER, 100
+
+end
+
+run_example1()
 
 
+--======================================================--
 --== Create Widget, inline style
 
-text2 = Widgets.newText{
-	text="inline style",
+function run_example2()
 
-	style={
-		width=225,
-		height=35,
+	local txt2
 
-		align='right',
-		fontSize=26,
-		marginX=5,
-		fillColor={0.5,0,0.25},
-		textColor={1,0,0},
+	txt2 = Widgets.newText{
+		text="inline style",
+
+		style={
+			width=225,
+			height=35,
+
+			align='right',
+			fontSize=26,
+			marginX=5,
+			fillColor={0.5,0,0.25},
+			textColor={1,0,0},
+		}
 	}
-}
 
-text2.x = H_CENTER
-text2.y = V_CENTER
+	txt2.x = H_CENTER
+	txt2.y = V_CENTER
 
-text2.align='left'
--- text2.align='center'
--- text2.align='right'
+	txt2.align='left'
+	txt2.align='center'
+	-- txt2.align='right'
 
-text2:setAnchor( {0,0} )
--- text2:setAnchor( {0.5,0.5} )
--- text2:setAnchor( {1,1} )
-
-timer.performWithDelay( 1000, function()
-	text2.style=nil
-end)
+	txt2:setAnchor( {0,0} )
+	txt2:setAnchor( {0.5,0.5} )
+	-- txt2:setAnchor( {1,1} )
 
 
--- timer.performWithDelay( 1000, function()
--- 	print( "\n\nUpdate properties" )
-
--- 	text2.text="hamburger"
-
--- 	text2.width=300
--- 	text2.height=70
-
--- 	text2.align='right'
-
--- 	text2:setAnchor( {1,1} )
-
--- 	text2:setFillColor( 1,0,0,0.5 )
--- 	text2:setTextColor( 1,0,0,0.5 )
-
--- 	text2.font = native.systemFontBold
-
--- 	text2:setStrokeColor( 0,0,0,0.5 )
--- 	text2.strokeWidth = 4
-
--- 	text2.fontSize = 18
--- 	text2.marginX = 15
--- 	text2.marginY = 15
-
--- end)
-
--- timer.performWithDelay( 2000, function()
--- 	print( "\n\nUpdate properties" )
-
--- 	text2.x=100
-
--- 	text2.text="pizza"
-
--- 	text2.width=nil
--- 	text2.height=nil
-
--- 	text2.align='center'
--- 	text2:setAnchor( {0.5,0.5} )
-
--- 	text2.strokeWidth = 2
--- 	text2:setStrokeColor( 1,0,0,1 )
-
--- 	text2.font = native.systemFontBold
--- 	text2.fontSize = 30
-
--- 	text2:setFillColor( 0,0,0.5,0.8 )
--- 	text2:setTextColor( 1,0,1,0.5 )
-
--- end)
+	timer.performWithDelay( 1000, function()
+		txt2.style=nil -- clear style, to default
+	end)
 
 
+	timer.performWithDelay( 2000, function()
+		print( "\n\nUpdate properties" )
 
--- timer.performWithDelay( 2000, function()
--- 	text:removeSelf()
--- end)
+		transition.to( txt2, {time=5000, x=100, y=400})
+
+		txt2.text="hamburger"
+
+		txt2.width=300
+		txt2.height=70
+
+		txt2.align='right'
+
+		txt2:setAnchor( {1,1} )
+
+		txt2:setFillColor( 1,0,0,0.5 )
+		txt2:setTextColor( 1,0,0,0.5 )
+
+		txt2.font = native.systemFontBold
+
+		txt2:setStrokeColor( 0,0,0,0.5 )
+		txt2.strokeWidth = 4
+
+		txt2.fontSize = 18
+		txt2.marginX = 15
+		txt2.marginY = 15
+
+	end)
+
+	timer.performWithDelay( 3000, function()
+		print( "\n\nUpdate properties" )
+
+		-- txt2.x=100
+
+		txt2.text="pizza"
+
+		txt2.width=nil
+		txt2.height=nil
+
+		txt2.align='center'
+		-- txt2:setAnchor( {0.5,0.5} )
+
+		txt2.strokeWidth = 2
+		txt2:setStrokeColor( 1,0,0,1 )
+
+		txt2.font = native.systemFontBold
+		txt2.fontSize = 30
+
+		txt2:setFillColor( 0,0,0.5,0.8 )
+		txt2:setTextColor( 1,0,1,0.5 )
+
+	end)
 
 
---== create style, widget
+end
 
--- style = Widgets.newTextStyle{
--- 	name='my-text-style',
--- 	fillColor={1,0,0},
--- 	textColor={1,1,1}
--- }
-
--- text3 = Widgets.newText{
--- 	text="third style"
--- }
--- text3.style = style
--- text3.x, text3.y = 150, 100
-
--- text3:setFillColor( 1,0.5,0.5,0.5 )
--- text3:setTextColor( 1,0,0,0.5 )
--- text3.align='left'
--- text3.width=200
-
--- text3 = Widgets.newText{
--- 	text="one two",
--- 	style=style
--- }
--- text3.x=100
--- text3.y=100
+-- run_example2()
 
 
+
+--======================================================--
+--== create style, apply to widget
+
+function run_example3()
+
+	local st1, txt3, txt4
+
+	st3 = Widgets.newTextStyle{
+		name='my-text-style',
+		fillColor={0.5,0.8,0.2},
+		marginX=5,
+		marginY=10,
+		textColor={0,0,0},
+	}
+
+	txt3 = Widgets.newText{
+		text="Text One"
+	}
+	txt3.style = st3
+	txt3.x, txt3.y = H_CENTER, V_CENTER-100
+
+
+	-- add another text widget, with same style
+
+	timer.performWithDelay( 1500, function()
+		print( "\n\nUpdate Properties" )
+
+		txt4 = Widgets.newText{
+			text="Text Two",
+			style=st3
+		}
+		txt4.x=H_CENTER
+		txt4.y=V_CENTER+100
+
+	end)
+
+	-- change some styles on one widget
+
+	timer.performWithDelay( 3000, function()
+		print( "\n\nUpdate Properties" )
+
+		txt3:setFillColor( 1,0.5,0.5,0.5 )
+		txt3:setTextColor( 1,0,0,0.5 )
+		txt3.align='left'
+		txt3.width=200
+
+		txt3:setTextColor( 0.5,0.2,0,0.5 )
+
+		-- st3:setFillColor(1,0,0)
+
+	end)
+
+
+	-- then reset both to original look
+
+	timer.performWithDelay( 4500, function()
+		print( "\n\nUpdate Properties" )
+
+		txt3:clearStyle()
+		txt4:clearStyle()
+	end)
+
+
+	-- remove a widget
+
+	timer.performWithDelay( 10000, function()
+		print( "\n\nRemoving Widget" )
+		txt3:removeSelf()
+	end)
+
+end
+
+-- run_example3()
