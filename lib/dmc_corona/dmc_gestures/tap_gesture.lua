94632e208f803b73b496a65dd3688e3de97d0aa4
dmc <my-github@davidmccuskey.com>
2015-03-29 18:03:01 -0600

    update module libs

--- lib/dmc_corona/dmc_gestures/tap_gesture.lua
+++ lib/dmc_corona/dmc_gestures/tap_gesture.lua
@@ -1,7 +1,7 @@
 --====================================================================--
 -- dmc_corona/dmc_gesture/tap_gesture.lua
 --
--- Documentation:
+-- Documentation: http://docs.davidmccuskey.com/dmc-gestures
 --====================================================================--
 
 --[[
@@ -64,6 +64,7 @@ local Objects = require 'dmc_objects'
 local Utils = require 'dmc_utils'
 
 local Gesture = require 'dmc_gestures.core.gesture'
+local Constants = require 'dmc_gestures.gesture_constants'
 
 
 
@@ -91,10 +92,7 @@ local TapGesture = newClass( Gesture, { name="Tap Gesture" } )
 
 --== Class Constants
 
-TapGesture.TYPE = 'tap'
-
-TapGesture.MAX_TAP_THRESHOLD = 300
-TapGesture.MIN_ACCURACY = 10
+TapGesture.TYPE = Constants.TYPE_TAP
 
 
 --- Event name constant.
@@ -120,10 +118,9 @@ TapGesture.MIN_ACCURACY = 10
 function TapGesture:__init__( params )
 	-- print( "TapGesture:__init__", params )
 	params = params or {}
-	if params.accuracy==nil then params.accuracy=TapGesture.MIN_ACCURACY end
-	if params.taps==nil then params.taps=1 end
-	if params.time==nil then params.time=TapGesture.MAX_TAP_THRESHOLD end
-	if params.touches==nil then params.touches=1 end
+	if params.accuracy==nil then params.accuracy=Constants.TAP_ACCURACY end
+	if params.taps==nil then params.taps=Constants.TAP_TAPS end
+	if params.touches==nil then params.touches=Constants.TAP_TOUCHES end
 
 	self:superCall( '__init__', params )
 	--==--
@@ -132,13 +129,9 @@ function TapGesture:__init__( params )
 
 	self._min_accuracy = params.accuracy
 	self._req_taps = params.taps
-	self._max_time = params.time
 	self._req_touches = params.touches
 
-	self._tap_count = 0 -- how many we've seen
-	self._tap_timer = nil
-
-	self._fail_timer = nil
+	self._tap_count = 0 -- how many taps
 
 end
 
@@ -150,7 +143,6 @@ function TapGesture:__initComplete__()
 	--== use setters
 	self.accuracy = self._min_accuracy
 	self.taps = self._req_taps
-	self.time = self._max_time
 	self.touches = self._req_touches
 end
 
@@ -242,23 +234,6 @@ function TapGesture.__setters:taps( value )
 end
 
 
---- the maximum time between tap touches in milliseconds (number).
--- this time is more important when doing multiple-tap sequences, eg double-tap. if any tap occurs after this time has elapsed, then the gesture fails. note: the timer is reset upon each valid tap.
---
--- @function .time
--- @usage print( gesture.time )
--- @usage gesture.time = 400
---
-function TapGesture.__getters:time()
-	return self._max_time
-end
-function TapGesture.__setters:time( value )
-	assert( type(value)=='number' and value>150 )
-	--==--
-	self._max_time = value
-end
-
-
 --- the minimum number of touches required to recognize (number).
 -- this is used to specify the number of fingers required for each tap, eg a two-fingered single-tap, three-fingered double-tap.
 --
@@ -285,57 +260,6 @@ function TapGesture:_do_reset()
 	-- print( "TapGesture:_do_reset" )
 	Gesture._do_reset( self )
 	self._tap_count=0
-	self:_stopAllTimers()
-end
-
-
-function TapGesture:_stopFailTimer()
-	-- print( "TapGesture:_stopFailTimer" )
-	if not self._fail_timer then return end
-	timer.cancel( self._fail_timer )
-	self._fail_timer=nil
-end
-
-function TapGesture:_startFailTimer()
-	-- print( "TapGesture:_startFailTimer", self )
-	self:_stopFailTimer()
-	local time = self._max_time
-	local func = function()
-		timer.performWithDelay( 1, function()
-			self:gotoState( TapGesture.STATE_FAILED )
-			self._fail_timer = nil
-		end)
-	end
-	self._fail_timer = timer.performWithDelay( time, func )
-end
-
-
-
-function TapGesture:_stopTapTimer()
-	-- print( "TapGesture:_stopTapTimer" )
-	if not self._tap_timer then return end
-	timer.cancel( self._tap_timer )
-	self._tap_timer=nil
-end
-
-function TapGesture:_startTapTimer()
-	-- print( "TapGesture:_startTapTimer", self )
-	self:_stopFailTimer()
-	self:_stopTapTimer()
-	local time = self._max_time
-	local func = function()
-		timer.performWithDelay( 1, function()
-			self:gotoState( TapGesture.STATE_FAILED )
-			self._tap_timer = nil
-		end)
-	end
-	self._tap_timer = timer.performWithDelay( time, func )
-end
-
-
-function TapGesture:_stopAllTimers()
-	self:_stopFailTimer()
-	self:_stopTapTimer()
 end
 
 
@@ -351,13 +275,16 @@ function TapGesture:touch( event )
 	Gesture.touch( self, event )
 
 	local phase = event.phase
-	local touch_count = self._touch_count
 
 	if phase=='began' then
-		self:_startFailTimer()
 		local r_touches = self._req_touches
+		local touch_count = self._touch_count
+
+		self:_startFailTimer()
+		self._gesture_attempt=true
+
 		if touch_count==r_touches then
-			self:_startTapTimer()
+			self:_startGestureTimer()
 		elseif touch_count>r_touches then
 			self:gotoState( TapGesture.STATE_FAILED )
 		end
@@ -365,6 +292,7 @@ function TapGesture:touch( event )
 	elseif phase=='moved' then
 		local _mabs = mabs
 		local accuracy = self._min_accuracy
+
 		if _mabs(event.xStart-event.x)>accuracy or _mabs(event.yStart-event.y)>accuracy then
 			self:gotoState( TapGesture.STATE_FAILED )
 		end
@@ -373,11 +301,13 @@ function TapGesture:touch( event )
 		self:gotoState( TapGesture.STATE_FAILED )
 
 	else -- ended
+		local touch_count = self._touch_count
 		local r_taps = self._req_taps
 		local taps = self._tap_count
-		if self._tap_timer and touch_count==0 then
+
+		if self._gesture_timer and touch_count==0 then
 			taps = taps + 1
-			self:_stopTapTimer()
+			self:_stopGestureTimer()
 		end
 		if taps==r_taps then
 			self:gotoState( TapGesture.STATE_RECOGNIZED )
@@ -397,22 +327,7 @@ end
 --== State Machine
 
 
---== State Recognized ==--
-
-function TapGesture:do_state_recognized( params )
-	-- print( "TapGesture:do_state_recognized" )
-	self:_stopAllTimers()
-	Gesture.do_state_recognized( self, params )
-end
-
-
---== State Failed ==--
-
-function TapGesture:do_state_failed( params )
-	-- print( "TapGesture:do_state_failed" )
-	self:_stopAllTimers()
-	Gesture.do_state_failed( self, params )
-end
+-- none
 
 
 
